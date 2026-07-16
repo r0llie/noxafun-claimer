147f5406b4d6702110aaa50725947d3594434841
unknown <rollieboss123@gmail.com>
2026-07-17 02:04:15 +0300

    Prepare private site deployment

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,27 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  plugins: [
+    {
+      name: "sites-worker-entry",
+      generateBundle() {
+        this.emitFile({
+          type: "asset",
+          fileName: "server/index.js",
+          source: `export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+    if (url.pathname.startsWith("/api/noxa/")) {
+      const upstream = new URL(url.pathname.replace("/api/noxa/", "/catalog/rh/"), "https://fun.noxa.eth.link");
+      return fetch(upstream, { headers: { accept: "*/*", referer: "https://fun.noxa.eth.link/" } });
+    }
+    return env.ASSETS.fetch(request);
+  }
+};`
+        });
+      }
+    }
+  ],
   server: {
     proxy: {
       "/api/noxa": {
